1018a40cd8a1acee2e8aecb1b97915f165eb6c6f
vamshi <k.vamshi2008@gmail.com>
2015-01-24 20:40:24 -0800

    Added support for location, tokens

--- worker.py
+++ worker.py
@@ -9,14 +9,17 @@ mongo = MongoClient()
 db = mongo['mydb']
 # Get the collection
 
-# Get the collection for the jobs_referred
-jobs_reffered_col = db.jobs_refd
-
 
 # Fetch the jobs which are there in the springrole
 
 
-def send_msg( emp_id, ref_id ):
+def send_msg( p, title, job_id ):
+	print "-----------------------------"
+	print p
+	print title
+	print job_id
+	print "-----------------------------"
+	print "SENTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT for "+str(p)+" job :"+str(job_id)
 	return True
 	# ref id is accesstoken
 	# emp id is the linkedin generated msg id	
@@ -24,23 +27,24 @@ def send_msg( emp_id, ref_id ):
 def get_skill_structure():
 	skills = {};
 	# Parse the json and return the dictionary of the skills
-	col = db.skilly
-	skill_cur = col.find({}, { "friends.empName": 1, "friends.skill1" : 1, "friends.skill2" : 2 , "friends.empEmail":1, "friends.jobs":1, "refId":1 })
+	col = db.springskills
+	skill_cur = col.find({}, { "friends.empName": 1, "friends.skill1" : 1, "friends.skill2" : 2 , "friends.empId":1, "friends.location":1, "refId":1, "refName":1, "refToken":1 })
 	
 	for i in skill_cur:
 		
 		# Get the refereer id
-		ref_id = i["refId"]
+		ref_token = i["refToken"]
+		refname = i["refName"]
+		refId = i["refId"]
 		
 		for j in i["friends"]:
 			emp_name = j["empName"]
-			# TODO iemail to be changed
-			emp_Id = j["empEmail"]
+			emp_Id = j["empId"]
 			s1 = j["skill1"]
 			s2 = j["skill2"]
-			job_arr = j["jobs"]
+			loc = j["location"]
 			
-			t = ( emp_name, emp_Id, ref_id );
+			t = ( emp_name, emp_Id, ref_token , refname, refId, loc );
 	
 			s1 = str(s1)
 			s2 = str(s2)
@@ -96,35 +100,41 @@ while True:
 			for p in people :
 
 				# For each person p, check if the job request is already sent
+				# p[0] --> empName
+				# p[1] --> empId
+				# p[2] --> refToken
+				# p[3] --> refName
+				# p[4] --> refId
+				# p[5] --> location
+				col1 = db.springjobs
 				
-				col1 = db.emp_jobs_new
-				
-				print "Person : "+str(p[0])+" referred by "+str(p[2])+" his/her email :"+str(p[1])
+				print "Person : "+str(p[0])+" referred by "+str(p[3])+" his/her email :"+str(p[1])
 
-				job_r_cur = col1.find({ "empEmail" : p[1] }, { "jobs_referred":1 })
+				job_r_cur = col1.find({ "empId" : p[1] }, { "jobs_referred":1 })
 				
 				if job_r_cur.count() == 0 :
 					# the emp object is not present
 					print "His/her object is not found in the referrals sent DS"
 					print "Creating one"
 					# create an entry and add it to the db collection emp_jobs_new	
-					post_id = db.emp_jobs_new.insert( { "empEmail" : p[1], "jobs_referred": [ ] } )
+					post_id = db.springjobs.insert( { "empId" : p[1], "jobs_referred": [ ] } )
 					print "Insert Sucess"
 					print post_id
 				
 				# Emp entry is present now
 				# Now check if the emp_object has the job
+				job_r_cur = col1.find({ "empId" : p[1] }, { "jobs_referred":1 })
 				
 				print "Now checking if the job "+str(job_id)+" is already there in "+str(job_r_cur[0]["jobs_referred"])
 
 				if job_id not in job_r_cur[0]["jobs_referred"]:
 					# if job id not present
 					print "its not present"
-					if send_msg(p[1],p[2]):
+					if send_msg(p, job["title"], job_id):
 						# Success
 						print "Sent the message"
 						# populate the new job to the database
-						db.emp_jobs_new.update({ "empEmail":p[1]}, { '$push' : { "jobs_referred" : job_id } })
+						db.springjobs.update({ "empId":p[1]}, { '$push' : { "jobs_referred" : job_id } })
 				else:
 					print "Job referral "+str(job_id)+" Already sent for "+str(p[0])
 				# The message has been sent, if its not already sent before
